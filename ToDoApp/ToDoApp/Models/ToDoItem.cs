e200049a5b183aecaa27e105ea4cc15a79f3a16c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-10 08:11:13 +0900

    主キーとなるプロパティを追加

--- ToDoApp/ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/ToDoApp/Models/ToDoItem.cs
@@ -11,6 +11,11 @@ namespace ToDoApp.Models
 	public class ToDoItem
 	{
 		/// <summary>
+		/// ID
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
 		/// タイトル
 		/// </summary>
 		public string Title { get; set; }
@@ -35,30 +40,35 @@ namespace ToDoApp.Models
 			{
 				new ToDoItem()
 				{
+					Id = 1,
 					Title = "Title1",
 					Body = "Body1",
 					Date = DateTime.Now
 				},
 				new ToDoItem()
 				{
+					Id = 2,
 					Title = "Title2",
 					Body = "Body2",
 					Date = DateTime.Now
 				},
 				new ToDoItem()
 				{
+					Id = 3,
 					Title = "Title3",
 					Body = "Body3",
 					Date = DateTime.Now
 				},
 				new ToDoItem()
 				{
+					Id = 4,
 					Title = "Title4",
 					Body = "Body4",
 					Date = DateTime.Now
 				},
 				new ToDoItem()
 				{
+					Id = 5,
 					Title = "Title5",
 					Body = "Body5",
 					Date = DateTime.Now
