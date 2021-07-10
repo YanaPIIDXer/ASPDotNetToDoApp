d336b7c4f955571f27204247d0dbee796efe8e1f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-10 09:33:35 +0900

    DB接続関係の記述

--- ToDoApp/Startup.cs
+++ ToDoApp/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoApp
 {
@@ -24,6 +26,7 @@ namespace ToDoApp
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+			services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
